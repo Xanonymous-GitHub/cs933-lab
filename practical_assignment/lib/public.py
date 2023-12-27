5125f122d0202329c962d33572788e860ed08b1f
Xanonymous <trusaidlin@gmail.com>
2023-12-27 16:21:18 +0000

    feat: 🎸 roughly find bounding boxes of resister3

--- practical_assignment/lib/public.py
+++ practical_assignment/lib/public.py
@@ -1,19 +1,25 @@
 from typing import Final
 
+import cv2
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
+from PIL import ImageEnhance
 from skimage import measure
+from skimage.restoration import inpaint
 
-from ._file import _FileAgent
 from ._box import find_bounding_box_from
+from ._file import _FileAgent
 
 __all__ = [
     "np",
     "plt",
     "mpl",
     "measure",
+    "cv2",
     "db",
+    "inpaint",
+    "ImageEnhance",
     "find_bounding_box_from",
 ]
 
