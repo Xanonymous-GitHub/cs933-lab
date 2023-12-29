f2a6386ee7895aaa9c58a96e302270f2e71f0a38
Xanonymous <trusaidlin@gmail.com>
2023-12-29 19:15:08 +0000

    fix: 🐛 wrong image plot title

--- practical_assignment/lib/_presentation.py
+++ practical_assignment/lib/_presentation.py
@@ -7,7 +7,7 @@ def show_images_in_row(images: [np.ndarray], /, *, fig_size: tuple[int, int] = (
     _, _axs = plt.subplots(1, len(images), figsize=fig_size)
     for n, component in enumerate(images):
         _axs[n].imshow(component)
-        _axs[n].set_title(f'Resistor {n + 1}')
+        _axs[n].set_title(f'Image {n + 1}')
 
     plt.show()
 
